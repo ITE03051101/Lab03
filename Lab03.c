7da7dbecf72af405f05235f74f29856c17dfe47d
ITE03051101 <secret20631@gmail.com>
2016-09-19 17:36:17 +0800

    fuck

---
+++ Lab03.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+
+int main (void)
+{
+	int a,b,c,d,e,f,g,h,i,j;
+	printf("enter ten number eith ten space\n");
+	scanf("%d",&a);
+scanf("%d %d %d %d %d %d %d %d %d %d",&a,&b,&c,&d,&e,&f,&g,&h,&i,&j);
+
+	printf("the number you enter\n");
+printf("%d\n",a);
+printf("%d\n",b);
+printf("%d\n",c);
+printf("%d\n",d);
+printf("%d\n",e);
+printf("%d\n",f);
+printf("%d\n",g);
+printf("%d\n",h);
+printf("%d\n",i);
+printf("%d\n",j);
+
+	
+	return 0;
+}
